3c43cd4b3d00e16837d050ee47e6c145df6adbdf
Muhammad Sulaiman <trendingofyt@gmail.com>
2023-04-18 23:27:52 +0500

    changes added

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -33,14 +33,13 @@ const Slider = (props) => {
 
   return (
     <>
-   <div className="flex items-center justify-between gap-6 py-5">
+   <div className="flex items-center justify-between gap-6 py-4">
    <div className='font-bold text-[30px]'>{props.title}</div>
-    {/* <div className="flex items-center gap-3">
+    <div className="flex items-center gap-3">
       <div ref={navigationPrevRef} className='flex items-center justify-center w-[45px] h-[45px] bg-blue-500 rounded-full transition hover:bg-blue-600 cursor-pointer'><svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.828 7.50005H16V9.50005H3.828L9.192 14.864L7.778 16.278L0 8.50005L7.778 0.722046L9.192 2.13605L3.828 7.50005Z" fill="white"/></svg></div>
       <div ref={navigationNextRef} className='flex items-center justify-center w-[45px] h-[45px] bg-blue-500 rounded-full transition hover:bg-blue-600 cursor-pointer'><svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.172 7.50005L6.808 2.13605L8.222 0.722046L16 8.50005L8.222 16.278L6.808 14.864L12.172 9.50005H0V7.50005H12.172Z" fill="white"/></svg></div>
-    </div> */}
+    </div>
 
-    <input type="text" placeholder='Search' className='w-full sm:w-[400px] bg-white px-2 py-2 rounded-md border-solid border border-blue-300 outline-blue-500' />
    </div>
 
    
@@ -105,7 +104,7 @@ const Slider = (props) => {
                     >
                       <div>
                         <button onClick={openModal} className='w-full'>
-                            <img src={parameter.image} className='grayscale select-none h-[calc(100vh-160px)] w-full object-cover object-bottom rounded-[14px]' alt="cover" />
+                            <img src={parameter.image} className='grayscale select-none h-[27vh] w-full object-cover object-bottom rounded-[14px]' alt="cover" />
                             <div className="flex items-center justify-between gap-2 mt-2 ">
                             <div className='font-[500] text-[19px] leading-[36px] uppercase'>{parameter.text}</div>
                             <div className=''><svg width="15" height="14" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.3389 4.552L2.86287 16.028L0.977539 14.1427L12.4522 2.66667H2.33887V0H17.0055V14.6667H14.3389V4.552Z" fill="black"/></svg></div>
