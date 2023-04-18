adca41c5dfb09ef93a2cfbb32d2f4c89938bd9d4
Muhammad Sulaiman <trendingofyt@gmail.com>
2023-04-18 19:42:15 +0500

    web_link

--- Bot/bot.js
+++ Bot/bot.js
@@ -7,7 +7,7 @@ const web_link = "https://caresoul.vercel.app/";
 bot.start((ctx) =>
   ctx.reply("Welcome :)))))", {
     reply_markup: {
-      keyboard: [[{ text: "web app", web_app: { url: "web_link" } }]],
+      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
     },
   })
 );
